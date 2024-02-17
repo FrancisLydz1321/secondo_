c26a3eb231e5b3ea12bb5307bd4c6cecf41ab4e2
Francis Lydzustre <francislydzustre1321@gmail.com>
2024-02-17 17:46:09 +0800

    qoiwueoqiwueoiqwueiqweuoeique

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // everything in flutter is a widget including this MyApp class; and every widget has a build() method; widgets are in a form of methods and these widgets gets nested. Typical functional programming;
   const MyApp({super.key});
 
   @override
@@ -14,10 +15,11 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
         home: MyHomePage(),
       ),
@@ -26,27 +28,77 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
+  // ChangeNotifier is a: state management
+  // property
   var current = WordPair.random();
+
+  // method
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners(); // state management thanks to ChangeNotifier
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    var appState = context
+        .watch<MyAppState>(); // watches reference of the MyAppState Class
+    var pair = appState.current; // section 5 new add;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-              'A random idea: asdadsdadadasddadadaddsdadaskslfjalkfja;slkfja;flkaj;laksfj;alkfj;aksfjas;kflajsfl;kasjf;laskfjas;lkfjas;lfkj'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-              onPressed: () {
-                print("This button has been pressedasadasdasd");
-              },
-              child: Text("NEXT!"))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random idea: randomly ngek ngek'),
+            // Text(appState.current.asLowerCase),
+            // Text(pair
+            //     .asLowerCase), // section 5 new add; pair. references the appstate declared above;
+            BigCard(pair: pair),
+            ElevatedButton(
+                onPressed: () {
+                  // print("This button has been pressedasadasdasd");
+                  appState
+                      .getNext(); // section 4: check MyAappState class method;
+                },
+                child: Text("NEXT!"))
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      // section 5: "!" or exclamation operator indicates for null safety operator
+      color: theme.colorScheme.onPrimary,
+    );
+
+    // return Text(pair.asLowerCase);
+    return Card(
+      color: theme.colorScheme
+          .primary, // section 5: uses the primary color of this application
+      child: Padding(
+          padding: const EdgeInsets.all(20),
+          // child: Text(pair.asLowerCase),
+          // child: Text(pair.asLowerCase, style: style),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: '${pair.first} ${pair.second}',
+          )),
     );
   }
 }
